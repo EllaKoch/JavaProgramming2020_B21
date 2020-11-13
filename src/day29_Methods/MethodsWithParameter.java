bc0bc486da00ff1776c984710296bcdef283ffc2
EllaKoch <ms.kosya555@gmail.com>
2020-11-13 12:09:35 -0500

    added class

---
+++ src/day29_Methods/MethodsWithParameter.java
@@ -0,0 +1,33 @@
+package day29_Methods;
+
+public class MethodsWithParameter {
+    public static void main(String[] args) {
+
+        int age = 20;
+        eligibleToBuyAlcohol(age);
+
+        eligibleToBuyAlcohol(30);
+
+        calculateAge(2018,2015);
+        calculateAge(1965,2020);
+
+
+    }
+
+    public static void eligibleToBuyAlcohol(int age){
+        if(age>=21){
+            System.out.println("Eligible to buy alcohol");
+        }else{
+            System.out.println("Not Eligible to buy alcohol");
+        }
+    }
+
+    public static void calculateAge(int birthYear, int currentYear) {
+        if (birthYear < currentYear) {
+            System.out.println("Your age is: " + (currentYear - birthYear) + " years old");
+        }else{
+            System.out.println("Invalid Entry");
+        }
+    }
+
+}
