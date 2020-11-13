0c7557bceaefabc8b3e9203c60df5d6de9440e4c
EllaKoch <ms.kosya555@gmail.com>
2020-11-13 15:53:22 -0500

    added class

---
+++ src/day29_Methods/VoidMethodsPractice3.java
@@ -0,0 +1,32 @@
+package day29_Methods;
+
+// eligibility to vote
+public class VoidMethodsPractice3 {
+    public static void main(String[] args) {
+
+        eligibleToVote("usa", 29, true);
+
+    }
+
+    public static void eligibleToVote(String citizenship, int age, boolean isAlive) {
+        if (citizenship.equalsIgnoreCase("USA")) {
+            if (isAlive) {
+                if (age >= 18) {
+                    System.out.println("You are eligible to vote!");
+                } else {
+                    System.err.println("You must be at least 18 years old to be eligible to vote");
+                }
+            } else {
+                System.err.println("You must come back to life in order to vote");
+            }
+
+
+        } else {
+            System.err.println("You must be a citizen of USA to be eligible to vote");
+        }
+
+
+    }
+
+
+}
