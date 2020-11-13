72f6100ca2676dda23ee26a5ecbe0f1095f3fa3d
EllaKoch <ms.kosya555@gmail.com>
2020-11-13 16:18:49 -0500

    new class

---
+++ src/day29_Methods/Calculator.java
@@ -0,0 +1,49 @@
+package day29_Methods;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        calculator(scan.nextDouble(), scan.next().charAt(0), scan.nextDouble());
+
+        calculator(20, '*', 2);
+
+    }
+
+    public static void calculator(double n1, char operator, double n2) {
+
+        boolean isValid = operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%';
+
+        if (isValid) {
+
+            switch (operator) {
+                case '+':
+                    System.out.println("Addition: " + (n1 + n2));
+                    break;
+                case '-':
+                    System.out.println("Subtraction: " + (n1 - n2));
+                    break;
+                case '*':
+                    System.out.println("Multiplication: " + (n1 * n2));
+                    break;
+                case '/':
+                    System.out.println("Division: " + (n1 / n2));
+                    break;
+                default:
+                    System.out.println("Remainder: " + (n1 % n2));
+                    break;
+
+            }
+
+
+        } else {
+            System.err.println("Invalid Operator");
+        }
+
+    }
+
+
+}
--- src/day29_Methods/CalculatorVoidMethod.java
+++ src/day29_Methods/CalculatorVoidMethod.java
@@ -2,10 +2,10 @@ package day29_Methods;
 
 public class CalculatorVoidMethod {
     public static void main(String[] args) {
-        calculator(5.5, 5.6, '*');
+        calculator(5.5, '*',5.6 );
     }
 
-    public static void calculator(double n1, double n2, char operator) {
+    public static void calculator(double n1, char operator, double n2 ) {
         double result = 0;
         switch (operator) {
             case '+':
