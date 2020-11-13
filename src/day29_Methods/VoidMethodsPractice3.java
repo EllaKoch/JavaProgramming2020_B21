4070ae67145057b4f5bd5c2f6bc3235945e6f561
EllaKoch <ms.kosya555@gmail.com>
2020-11-13 15:58:46 -0500

    correction

--- src/day29_Methods/VoidMethodsPractice3.java
+++ src/day29_Methods/VoidMethodsPractice3.java
@@ -4,25 +4,27 @@ package day29_Methods;
 public class VoidMethodsPractice3 {
     public static void main(String[] args) {
 
-        eligibleToVote("usa", 29, true);
+        eligibleToVote("Elvira","usa", 29, true);
+        eligibleToVote("Bella","Russia", 35,true);
+        eligibleToVote("Aaron", "usa", 135, false);
 
     }
 
-    public static void eligibleToVote(String citizenship, int age, boolean isAlive) {
+    public static void eligibleToVote(String name ,String citizenship, int age, boolean isAlive) {
         if (citizenship.equalsIgnoreCase("USA")) {
             if (isAlive) {
                 if (age >= 18) {
-                    System.out.println("You are eligible to vote!");
+                    System.out.println(name+", you are eligible to vote!");
                 } else {
-                    System.err.println("You must be at least 18 years old to be eligible to vote");
+                    System.err.println(name+", you must be at least 18 years old to be eligible to vote");
                 }
             } else {
-                System.err.println("You must come back to life in order to vote");
+                System.err.println(name+", you must come back to life in order to vote");
             }
 
 
         } else {
-            System.err.println("You must be a citizen of USA to be eligible to vote");
+            System.err.println(name+", you must be a citizen of USA to be eligible to vote");
         }
 
 
