1b54a6f491f7d7de1014140751e1519e0c751adc
EllaKoch <ms.kosya555@gmail.com>
2020-11-13 10:36:52 -0500

    added class

---
+++ src/day29_Methods/Practice.java
@@ -0,0 +1,34 @@
+package day29_Methods;
+/*
+
+        step1: print hello 5 times  (for loop)
+        step2: print your name
+        step3: print hello 5 times  (for loop)
+        step4: print your school name
+        step5: print hello 5 times   (for loop)
+ */
+public class Practice {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <6; i++){
+            System.out.println("Hello");
+        }
+
+        System.out.println("Elvira");
+
+        for(int i = 1; i < 6; i++){
+            System.out.println("Hello");
+        }
+
+        System.out.println("Cybertek");
+
+
+        for(int i = 1; i < 6; i++){
+            System.out.println("Hello");
+        }
+
+
+
+    }
+
+}
